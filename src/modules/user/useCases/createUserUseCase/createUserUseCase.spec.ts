7eece06f3e036fde2c337472ff1fb4f447090255
Rafael Lima <srafalimacosta@gmail.com>
2025-02-28 20:40:21 -0300

    implement create user use case with password encryption and tests

---
+++ src/modules/user/useCases/createUserUseCase/createUserUseCase.spec.ts
@@ -0,0 +1,42 @@
+import { compare } from 'bcrypt';
+import { UserRepositoryInMemory } from '../../repositories/UserRepositoryInMemory';
+import { CreateUserUseCase } from './createUserUseCase';
+
+let createUserUseCase: CreateUserUseCase;
+let userRepositoryInMemory: UserRepositoryInMemory;
+
+describe('Create User', () => {
+  beforeEach(() => {
+    userRepositoryInMemory = new UserRepositoryInMemory();
+    createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
+  });
+
+  it('is able to create user', async () => {
+    expect(userRepositoryInMemory.users).toEqual([]);
+
+    const user = await createUserUseCase.execute({
+      email: 'email@email.com',
+      name: 'Fulano',
+      password: '123123',
+    });
+
+    expect(userRepositoryInMemory.users).toEqual([user]);
+  });
+
+  it('is able to create user with password encrypted', async () => {
+    const userPasswordWithoutEncryption = '123123';
+
+    const user = await createUserUseCase.execute({
+      email: 'email@email.com',
+      name: 'Fulano',
+      password: userPasswordWithoutEncryption,
+    });
+
+    const userHasPasswordEncrypted = await compare(
+      userPasswordWithoutEncryption,
+      user.password,
+    );
+
+    expect(userHasPasswordEncrypted).toBeTruthy();
+  });
+});
